f8cd64cd406aff5eda42d71840c3385598cb1461
glennmelkior <melkiorglenn@hotmail.com>
2020-08-15 13:27:21 +0700

    Initial Commit

---
+++ ProjectGaruda/ViewController.swift
@@ -0,0 +1,19 @@
+//  ViewController.swift
+//  ProjectGaruda
+//
+//  Created by Glenn Melkior on 8/15/20.
+//  Copyright © 2020 Glenn Melkior. All rights reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
